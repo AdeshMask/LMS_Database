51b2fa8d9d076a4e9f9fc776036eafe7566ce7a9
AdeshMask <adeshmaske255@gmail.com>
2022-08-25 19:52:17 +0530

    Added: All database tables which are required for LMS

---
+++ Candidate_BankDetails.sql
@@ -0,0 +1,15 @@
+create table Candidate_BankDetails
+( 
+Id int auto_increment PRIMARY KEY,
+Candidate_ID int,
+Name varchar(225),
+Account_Number bigint, 
+Ifsc_code bigint, 
+pan_number varchar(225), 
+addhaar_num bigint,
+creator_stamp date,
+creator_user varchar(225),
+foreign key (Candidate_ID) references FellowshipCandidate (Candidate_ID)
+);
+
+describe Candidate_BankDetails;
---
+++ LabThreshold.sql
@@ -0,0 +1,15 @@
+create table LabThreshold
+(
+Thresgold_Id int,
+Lab_Id int,
+Lab_Capacity int,
+Lead_Threshold int,
+Tdeation_Engg_Threshold varchar(60),
+Buddy_Engg_Threshold varchar(60),
+Status varchar(60),
+Creator_Stamp date,
+Creator_User varchar(60),
+Foreign key (Lab_Id) references Lab (Lab_ID)
+);
+desc LabThreshold;
+alter table LabThreshold ADD primary key(Thresgold_Id);
